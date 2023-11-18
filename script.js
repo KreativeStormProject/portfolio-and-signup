1fe636951420af7b7b34a8fc9a053ed7e061b3d4
Clinton-stack <56035975+Clinton-stack@users.noreply.github.com>
2023-11-18 11:59:29 +0100

    frist commit

---
+++ script.js
@@ -0,0 +1,38 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const form = document.getElementById('signup-form');
+    const createButton = document.getElementById('create-button');
+    const passwordInput = document.getElementById('password');
+    const confirmPasswordInput = document.getElementById('confirm_password');
+    const passwordMatchError = document.getElementById('password-match-error');
+  
+
+    form.addEventListener('input', function (event) {
+      const input = event.target;
+      const pattern = new RegExp(input.pattern);
+      var errorMessage = document.querySelector('.error-message[data-target="' + input.name + '"]');
+  
+      if (pattern.test(input.value) || !input.value.trim()) {
+          errorMessage.style.display = 'none';
+          input.classList.remove('error-color'); 
+      } else {
+          errorMessage.style.display = 'block';
+          input.classList.add('error-color'); 
+      }
+  });
+  
+  
+    confirmPasswordInput.addEventListener('input', function () {
+      if (confirmPasswordInput.value !== passwordInput.value) {
+        confirmPasswordInput.classList.add('password-mismatch');
+        passwordMatchError.style.display = 'block';
+        createButton.disabled = true
+
+      } else {
+        confirmPasswordInput.classList.remove('password-mismatch');
+        passwordMatchError.style.display = 'none';
+        createButton.disabled = false
+      }
+    });
+
+
+});
